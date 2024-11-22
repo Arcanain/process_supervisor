69e8fd8a04b1e9f47e9695ff5bcf4b5305de5f4b
nkn4ryu <shiryu.nakano@gmail.com>
2024-11-23 06:34:14 +0900

    [add]add swich process condisions, and condision judge logics

--- process_supervisor/process_manage_node.py
+++ process_supervisor/process_manage_node.py
@@ -3,8 +3,12 @@ import subprocess
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from nav_msgs.msg import Odometry
 import json
 import signal
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
 from ament_index_python.packages import get_package_share_directory
 
 class DynamicLaunchManager(Node):
@@ -19,39 +23,48 @@ class DynamicLaunchManager(Node):
         self._command_3 = "bash -c 'source ~/ros2_ws/install/setup.bash && ros2 launch pure_pursuit_planner pure_pursuit_planner.py'"
         
         # topic config TODO: make config file
-        self.ps_1_topic = "/hogehoge" # gps  /gnss_odom
-        self.ps_2_topic = "/fugafuga" # emcl /mcl_pose
-        
+        # GPS
+        self.ps_1_topic = "/swich_odom" # gps  /gnss_odom, to swich from gps to emcl
+        self.ps_1_topic_type = Odometry
+        # EMCL
+        self.ps_2_topic = "mcl_pose" # emcl /mcl_pose,  to swich from emcl to gps  
+        self.ps_2_topic_type = PoseWithCovarianceStamped
+
         # switch condition
-        # for TERMINATION of process 1 
-        
-        # for TERMINATION of process 2
-         
+        # GPS -> EMCL
+        self.th_gps = 1.0
+        self.target_x_1 = 0.0
+        self.target_y_1 = 0.0
+        # EMCL -> GPS
+        self.th_emcl = 1.0
+        self.target_x_2 = 0.0
+        self.target_y_2 = 0.0
         ### ############ ###
-        
+    
 
-
-        
         # subprocesses
         self.process_1 = None # start to the front of the roof: GPS
         self.process_2 = None # unter the roof: Lider, EMCL
         self.process_3 = None # roof to end: GPS
 
+
         ## Subscriber 
         # For process 1
         self.gps_subscriber = self.create_subscription(
-            String, # TODO change
+            self.ps_1_topic_type, # TODO change
             self.ps_1_topic, # topic name
             self.ps_1_callback,
             10
             )
+        
         # For process 2 
         self.subscriber = self.create_subscription(
-            String,
+            self.ps_2_topic_type,
             self.ps_2_topic,
             self.ps_2_callback,
             10
             )
+
 
         # 最初のプロセスを起動
         self.get_logger().info("Starting launch file...")
@@ -128,13 +141,21 @@ class DynamicLaunchManager(Node):
         return True # TODO 起動の成功判定
 
 
-    def ps_1_callback(self,msg):
+    def ps_1_callback(self,msg:Odometry):
         '''
         条件True: call stop process 1, after that, call launch_process2 
         False: 何もしない
         '''
+         # メッセージから座標を取得
+        current_x_gps = msg.pose.pose.position.x
+        current_y_gps = msg.pose.pose.position.y
+        
+        # ユークリッド距離を計算
+        distance = ((current_x_gps - self.target_x_1) ** 2 + (current_y_gps - self.target_y_1) ** 2) ** 0.5
+        #Reach = [True if distance <= 1.0 else False]
+
         # TODO condition check
-        if  (msg.data == 'true') and (self.process_1 is not None):
+        if  distance <= self.th_gps and (self.process_1 is not None):
             self.get_logger().info(f"Flag received on {self.ps_1_topic}. Switching to next process...")
             stop_result = self.stop_process_1()
             if stop_result:  # stop_process_1 Sucess
@@ -143,13 +164,21 @@ class DynamicLaunchManager(Node):
 
 
 
-    def ps_2_callback(self,msg):
+    def ps_2_callback(self,msg:PoseWithCovarianceStamped):
         '''
         条件True: call stop process 2, after that, call launch_process_3
         False: 何もしない
         '''
         # TODO condition check
-        if  (msg.data == 'true') and (self.process_2 is not None):
+
+        current_x_emcl = msg.pose.pose.position.x
+        current_y_emcl = msg.pose.pose.position.y
+
+        # ユークリッド距離を計算
+        distance = ((current_x_emcl - self.target_x_2) ** 2 + (current_y_emcl - self.target_y_2) ** 2) ** 0.5
+        #Reach = [True if distance <= 1.0 else False]
+
+        if  distance <= self.th_emcl and (self.process_2 is not None):
             self.get_logger().info(f"Flag received on {self.ps_2_topic}. Switching to next process...")
             stop_result = self.stop_process_2()
             if stop_result:  # stop_process_1 Sucess
