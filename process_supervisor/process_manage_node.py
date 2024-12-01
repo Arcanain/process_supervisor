534a5f172d222257237cf0fbfb56792157c08d85
shiryu-nakano <shiryu.nakano03@gmail.com>
2024-12-01 13:47:26 +0900

    [add] swich logic

--- process_supervisor/process_manage_node.py
+++ process_supervisor/process_manage_node.py
@@ -3,6 +3,7 @@ import subprocess
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from std_msgs.msg import Bool
 from nav_msgs.msg import Odometry
 import json
 import signal
@@ -18,17 +19,17 @@ class DynamicLaunchManager(Node):
         
         ### CONFIGURATION ###
         # command of launch each process
-        self._command_1 = "bash -c 'source ~/ros2_ws/install/setup.bash && ros2 launch pure_pursuit_planner pure_pursuit_planner.py'"
-        self._command_2 = "bash -c 'source ~/ros2_ws/install/setup.bash && ros2 launch arcanain_simulator simulator.py'"
-        self._command_3 = "bash -c 'source ~/ros2_ws/install/setup.bash && ros2 launch pure_pursuit_planner pure_pursuit_planner.py'"
+        self._command_1 = "bash -c 'source ~/ros2_ws/install/setup.bash && ros2 launch pure_pursuit_planner obstacle_simulation.py'"
+        self._command_2 = "bash -c 'source ~/ros2_ws/install/setup.bash && ros2 launch pure_pursuit_planner pure_pursuit_planner.py'"
+        self._command_3 = "bash -c 'source ~/ros2_ws/install/setup.bash && ros2 launch pure_pursuit_planner obstacle_simulation.py'"
         
         # topic config TODO: make config file
         # GPS
-        self.ps_1_topic = "/swich_odom" # gps  /gnss_odom, to swich from gps to emcl
-        self.ps_1_topic_type = Odometry
+        self.ps_1_topic = "/goal_status" # gps  /gnss_odom, to swich from gps to emcl
+        self.ps_1_topic_type = Bool
         # EMCL
-        self.ps_2_topic = "mcl_pose" # emcl /mcl_pose,  to swich from emcl to gps  
-        self.ps_2_topic_type = PoseWithCovarianceStamped
+        self.ps_2_topic = "goal_status2" # emcl /mcl_pose,  to swich from emcl to gps  
+        self.ps_2_topic_type = Bool
 
         # switch condition
         # GPS -> EMCL
@@ -141,21 +142,15 @@ class DynamicLaunchManager(Node):
         return True # TODO 起動の成功判定
 
 
-    def ps_1_callback(self,msg:Odometry):
+    def ps_1_callback(self,msg:Bool):
         '''
         条件True: call stop process 1, after that, call launch_process2 
         False: 何もしない
         '''
-         # メッセージから座標を取得
-        current_x_gps = msg.pose.pose.position.x
-        current_y_gps = msg.pose.pose.position.y
-        
-        # ユークリッド距離を計算
-        distance = ((current_x_gps - self.target_x_1) ** 2 + (current_y_gps - self.target_y_1) ** 2) ** 0.5
         #Reach = [True if distance <= 1.0 else False]
 
         # TODO condition check
-        if  distance <= self.th_gps and (self.process_1 is not None):
+        if  msg.data and (self.process_1 is not None):
             self.get_logger().info(f"Flag received on {self.ps_1_topic}. Switching to next process...")
             stop_result = self.stop_process_1()
             if stop_result:  # stop_process_1 Sucess
@@ -164,21 +159,14 @@ class DynamicLaunchManager(Node):
 
 
 
-    def ps_2_callback(self,msg:PoseWithCovarianceStamped):
+    def ps_2_callback(self,msg:Bool):
         '''
         条件True: call stop process 2, after that, call launch_process_3
         False: 何もしない
         '''
         # TODO condition check
 
-        current_x_emcl = msg.pose.pose.position.x
-        current_y_emcl = msg.pose.pose.position.y
-
-        # ユークリッド距離を計算
-        distance = ((current_x_emcl - self.target_x_2) ** 2 + (current_y_emcl - self.target_y_2) ** 2) ** 0.5
-        #Reach = [True if distance <= 1.0 else False]
-
-        if  distance <= self.th_emcl and (self.process_2 is not None):
+        if  msg.data and (self.process_2 is not None):
             self.get_logger().info(f"Flag received on {self.ps_2_topic}. Switching to next process...")
             stop_result = self.stop_process_2()
             if stop_result:  # stop_process_1 Sucess
